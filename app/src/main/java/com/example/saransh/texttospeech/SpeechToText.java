a64149161a240e1ff0aa828b1abb55998581aee8
Santwana <vermasantwana@gmail.com>
2016-10-02 04:59:36 +0530

    changed MainActivity.java and xml

---
+++ app/src/main/java/com/example/saransh/texttospeech/SpeechToText.java
@@ -0,0 +1,13 @@
+package com.example.saransh.texttospeech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechToText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_to_text);
+    }
+}
