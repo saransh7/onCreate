7c339b7a9af0b1adb075731d56bbf32ba1489759
Santwana <vermasantwana@gmail.com>
2016-10-02 05:10:21 +0530

    changed MainActivity.java and res

--- app/src/main/java/com/example/saransh/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/saransh/texttospeech/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.saransh.texttospeech;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,61 +12,35 @@ import android.widget.EditText;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
-    private TextToSpeech tts;
+public class MainActivity extends AppCompatActivity {
     private Button button;
-    private EditText editText;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tts = new TextToSpeech(this, this);
-        button = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.editText);
-        // button on click event
-        button.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
 
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                speakOut();
+                Intent i1 = new Intent(MainActivity.this, TextToSpeech.class);
+                startActivity(i1);
+
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i2 = new Intent(MainActivity.this, SpeechToText.class);
+                startActivity(i2);
+
             }
 
         });
     }
-
-    @Override
-    public void onDestroy() {
-        // Don't forget to shutdown tts!
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onDestroy();
-    }
-    @Override
-    public void onInit(int status) {
-
-        if (status == TextToSpeech.SUCCESS) {
-
-            int result = tts.setLanguage(Locale.US);
-
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "Unknown Language");
-            } else {
-                button.setEnabled(true);
-                speakOut();
-            }
-
-        } else {
-            Log.e("TTS", "Failed!");
-        }
-
-    }
-
-    private void speakOut() {
-        String text = editText.getText().toString();
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-    }
 }
+
